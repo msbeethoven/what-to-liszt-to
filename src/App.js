a9aa09267d83907343681cba4f95048c3ec2bc90
core <msbeethoven@users.noreply.github.com>
2021-06-28 14:56:05 -0400

    Basics of biographical information (birth date) from dynamic router set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 //import Composers from './components/Composers';
 import Mood from './components/Mood';
+import RouterExample from './components/RouterExample'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <RouterExample /> */}
         <Mood />
         {/* <Composers /> */}
       </header>
--- src/components/ComposerResults.js
+++ src/components/ComposerResults.js
@@ -9,10 +9,11 @@ import {
 import ComposerCard from "./ComposerCard";
 
 function ComposerResults(props) {
+
   return (
     <Router>
     <div>
-      <nav>
+      
       {console.log(
         "wig",
         props.whatever
@@ -21,25 +22,29 @@ function ComposerResults(props) {
       <ul>
         {props.whatever.map((comp) => (
           <li key={comp.id}>
-            <p>{comp.id}</p>
             <Link to={`${comp.id}`}>{comp.complete_name}</Link>
-            <p>{comp.complete_name}</p>
           </li>
         ))}
       </ul>
-      </nav>
-      <Switch>
-        <Route path="/149">
-          <Test />
+   
+      {props.whatever.map((comp) => (
+        <Switch>
+          {console.log('tf', comp.id)}
+        <Route path={`/${comp.id}`}>
+          <About chicken={comp}/>
         </Route>
       </Switch>
+      ))}
+      
     </div>
     </Router>
   );
 }
 
-function Test() {
-  return <h2>testing this baby out</h2>
+function About(props) {
+  console.log('now what', props)
+  return <h2>{props.chicken.birth}</h2>;
 }
+
 
 export default ComposerResults;
